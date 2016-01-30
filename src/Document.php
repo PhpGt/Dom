47af44abee04745ee57e4fc31c7317d8237f63f4
Greg Bowler <greg.bowler@g105b.com>
2016-01-30 12:23:38 +0000

    PHPDoc for document properties

--- src/Document.php
+++ src/Document.php
@@ -1,6 +1,29 @@
 <?php
 namespace g105b\Dom;
 
+/**
+ * @property string $characterSet
+ * @property string $contentType
+ * @property-read string $doctype The document type definition (DTD).
+ * @property-read Element $documentElement The direct child of the
+ * document. This is normally the HTML element.
+ * @property-read NodeList $anchors List of all anchors within the document.
+ *  Anchors are <a> elements with a `name` attribute.
+ * @property-read Element $body The <body> element.
+ * @property-read NodeList $forms List of all <form> elements.
+ * @property-read Element $head The <head> element.
+ * @property-read NodeList $images List of all <img> elements.
+ * @property-read NodeList $links List of all links within the document.
+ *  Links are <a> elements with an `href` attribute.
+ * @property-read NodeList $scripts List of all <script> elements.
+ * @property-read NodeList $styleSheets List of all stylesheets within the
+ *  document. Stylesheets are <link> elements with attribute `rel=stylesheet`
+ * @property string $title The document title.
+ */
 class Document extends Node {
+
+public function __construct() {
+
+}
 
 }#
