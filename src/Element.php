9825d15a1c2465ecf77a39bd16cc2b20146950b6
Greg Bowler <greg.bowler@g105b.com>
2016-01-31 20:51:46 +0000

    Element::remove

--- src/Element.php
+++ src/Element.php
@@ -3,4 +3,12 @@ namespace g105b\Dom;
 
 class Element extends \DOMElement {
 
+/**
+ * Removes the object from the tree it belongs to.
+ * @return void
+ */
+public function remove() {
+	$this->parentNode->removeChild($this);
+}
+
 }#
--- test/Document.test.php
+++ test/Document.test.php
@@ -10,4 +10,16 @@ public function testInheritance() {
 	$this->assertInstanceOf("g105b\Dom\Element", $document->documentElement);
 }
 
+public function testRemoveElement() {
+	$document = new Document(self::HTML);
+
+	$h1List = $document->getElementsByTagName("h1");
+	$this->assertEquals(1, $h1List->length);
+	$h1 = $h1List->item(0);
+	$h1->remove();
+
+	$h1List = $document->getElementsByTagName("h1");
+	$this->assertEquals(0, $h1List->length);
+}
+
 }#
