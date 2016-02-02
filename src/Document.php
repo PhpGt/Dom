98bd07eb4bb25fc8f3b8aa56db24c842c816a6bc
Greg Bowler <greg.bowler@g105b.com>
2016-02-02 22:32:42 +0000

    Remove element map

--- src/Document.php
+++ src/Document.php
@@ -3,8 +3,6 @@ namespace phpgt\dom;
 
 class Document extends \DOMDocument {
 
-private $elementMap = [];
-
 public function __construct() {
 	parent::__construct("1.0", "utf-8");
 	$this->registerNodeClass("\DOMNode", "\phpgt\dom\Node");
@@ -15,21 +13,6 @@ public function __construct() {
 	$this->registerNodeClass("\DOMDocumentType", "\phpgt\dom\DocumentType");
 	$this->registerNodeClass("\DOMCharacterData", "\phpgt\dom\CharacterData");
 	$this->registerNodeClass("\DOMText", "\phpgt\dom\Text");
-}
-
-/**
- * Returns an instance of Element or one of its extensions from any given Node.
- *
- * @param Node $node The Node to obtain the Element for
- * @return Element An Element or extension of Element with specific
- * functionality relating to its Element type
- */
-public function getNodeElement(Node $node):Element {
-	$hash = spl_object_hash($node);
-
-	if(!isset($this->elementMap[$node])) {
-		$this->elementMap[$node] = $node;
-	}
 }
 
 };
