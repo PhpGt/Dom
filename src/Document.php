41a46b2f58151dbbae0d9a2adfd2a3eefd0d5c4a
Greg Bowler <greg.bowler@g105b.com>
2016-02-02 18:17:10 +0000

    Register all classes

--- src/Document.php
+++ src/Document.php
@@ -3,10 +3,18 @@ namespace phpgt\dom;
 
 class Document extends \DOMDocument {
 
+private $elementMap = [];
+
 public function __construct() {
 	parent::__construct("1.0", "utf-8");
-	$this->registerNodeClass("\DOMElement", "\phpgt\dom\Element");
 	$this->registerNodeClass("\DOMNode", "\phpgt\dom\Node");
+	$this->registerNodeClass("\DOMElement", "\phpgt\dom\Element");
+	$this->registerNodeClass("\DOMAttr", "\phpgt\dom\Attr");
+	$this->registerNodeClass("\DOMDocumentFragment",
+		"\phpgt\dom\DocumentFragment");
+	$this->registerNodeClass("\DOMDocumentType", "\phpgt\dom\DocumentType");
+	$this->registerNodeClass("\DOMCharacterData", "\phpgt\dom\CharacterData");
+	$this->registerNodeClass("\DOMText", "\phpgt\dom\Text");
 }
 
 };
