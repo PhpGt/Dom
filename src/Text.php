5d516e900f5dd27d73fce2e691a9de1e6228a9e6
Greg Bowler <greg.bowler@g105b.com>
2016-02-03 22:08:28 +0000

    Remove already-declared method

--- src/Text.php
+++ src/Text.php
@@ -13,19 +13,4 @@ namespace phpgt\dom;
  * The Node.normalize() method merges adjacent Text objects back into a single
  * node for each block of text.
  */
-class Text extends \DOMText {
-
-/**
- * Replaces the text of the node and all of its logically adjacent text nodes
- * with the specified text. The replaced nodes are removed, including the
- * current node, unless it was the recipient of the replacement text.
- *
- * @param string $content Text to replace current Text Node with
- * @return Text|null The text node which received the replacement text, or
- * null if the replacement text is an empty string
- */
-public function replaceWholeText(string $content) {
-	// TODO.
-}
-
-}#
+class Text extends \DOMText {}#
