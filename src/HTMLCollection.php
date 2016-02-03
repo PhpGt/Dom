c4bcc5b0872d9b4d944b3d2cd81531f26f90b291
Greg Bowler <greg.bowler@g105b.com>
2016-02-03 17:29:06 +0000

    Implement HTMLCollection methods

--- src/HTMLCollection.php
+++ src/HTMLCollection.php
@@ -40,6 +40,32 @@ public function item(int $index) {
 	return null;
 }
 
+/**
+ * @param string $name Returns the specific Node whose ID or, as a fallback,
+ * name matches the string specified by $name. Matching by name is only done
+ * as a last resort, and only if the referenced element supports the name
+ * attribute.
+ *
+ * @return Element|null
+ */
+public function namedItem(string $name) {
+	$namedElement = null;
+
+	// Iterating all elements is costly. Room for improvement here?
+	foreach ($this as $element) {
+		if($element->getAttribute("id") === $name) {
+			return $element;
+		}
+
+		if(is_null($namedElement)
+		&& $element->getAttribute("name") === $name) {
+			$namedElement = $element;
+		}
+	}
+
+	return $namedElement;
+}
+
 public function __get($name) {
 	$methodName = "prop_$name";
 	if(method_exists($this, $methodName)) {
