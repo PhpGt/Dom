352262caee7aa6d090c22615bec19aafee35d3ee
Greg Bowler <greg.bowler@g105b.com>
2016-02-03 08:19:03 +0000

    Remove unused code

--- src/Node.php
+++ src/Node.php
@@ -3,15 +3,4 @@ namespace phpgt\dom;
 
 class Node extends \DOMNode {
 
-public function __construct() {
-	parent::__construct();
-}
-
-/**
- * Gets the unique hash used to identify the Node within its current Document.
- */
-public function getHash() {
-	return spl_object_hash($this);
-}
-
 }#
