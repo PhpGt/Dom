276a46833146bb93db2d6cb8158dea60fef95896
Greg Bowler <greg.bowler@g105b.com>
2016-07-08 18:18:13 +0100

    test TokenList::item

--- test/TokenList.test.php
+++ test/TokenList.test.php
@@ -1,8 +1,14 @@
 <?php
-namespace phpgt\dom\test;
+namespace phpgt\dom;
 
 class TokenListTest extends \PHPUnit_Framework_TestCase {
 
-
+public function testItem() {
+	$document = new HTMLDocument(test\Helper::HTML_MORE);
+	$h2 = $document->getElementById("who");
+	$tokenList = new TokenList($h2, "class");
+	$this->assertEquals("h-who", $tokenList->item(0));
+	$this->assertEquals("m-test", $tokenList->item(2));
+}
 
 }#
