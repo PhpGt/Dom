d88fbb3c12144efbdaadf1c96988b4cf87e367a6
James Fellows <james.fellows@finanscapes.com>
2017-03-23 11:20:39 +0000

    Add additional DocumentFragment tests

--- test/DocumentFragment.test.php
+++ test/DocumentFragment.test.php
@@ -3,8 +3,14 @@ namespace Gt\Dom;
 
 class DocumentFragmentTest extends \PHPUnit_Framework_TestCase {
 
-public function testQuerySelector() {
-	$document = new HTMLDocument();
+    const DOC_CONTENT_BEFORE_INSERT = "<!doctype html><body>"
+    . "<div><ul><li>outOfScope</li></ul></div>"
+    . "<span id='replaceWithSUT'></span>"
+    . "<div><ul><li>outOfScope</li></ul></div>"
+    . "</body>";
+
+public function testQuerySelectorBeforeBeforeAddingToDocument() {
+    $document = new HTMLDocument(self::DOC_CONTENT_BEFORE_INSERT);
 	$fragment = $document->createDocumentFragment();
 
 	$expectedFirstLi = null;
@@ -24,8 +30,8 @@ public function testQuerySelector() {
 	$this->assertSame($expectedFirstLi, $actualFirstLi);
 }
 
-public function testQuerySelectorAll() {
-	$document = new HTMLDocument();
+public function testQuerySelectorAllBeforeAddingToDocument() {
+    $document = new HTMLDocument(self::DOC_CONTENT_BEFORE_INSERT);
 	$fragment = $document->createDocumentFragment();
 
 	$expectedCount = 0;
@@ -41,26 +47,57 @@ public function testQuerySelectorAll() {
 	$this->assertCount($expectedCount, $liCollection);
 }
 
-public function testAppends() {
+public function testAppendsToDocument() {
 	$document = new HTMLDocument("<!doctype html><body><ul></ul></body>");
 	$fragment = $document->createDocumentFragment();
 
 	$expectedCount = 0;
+    $expectedFirstLi = null;
 	foreach(["Firefox", "Chrome", "Opera", "Safari", "Internet Explorer"]
 	as $browser) {
 		$expectedCount++;
 		$li = $document->createElement("li");
 		$li->textContent = $browser;
 		$fragment->appendChild($li);
+
+        if(is_null($expectedFirstLi)) {
+            $expectedFirstLi = $li;
+        }
 	}
 
 	$ul = $document->querySelector("ul");
 	$ul->appendChild($fragment);
 
-	$this->assertCount(
-		$expectedCount,
-		$document->querySelectorAll("body>ul>li")
-	);
+    $actualResult = $document->querySelectorAll("body>ul>li");
+    $this->assertCount($expectedCount, $actualResult);
+	$this->assertSame($expectedFirstLi, $actualResult[0]);
+}
+
+public function testQuerySelectorAfterAddingToDocument() {
+    $document = new HTMLDocument(
+        self::DOC_CONTENT_BEFORE_INSERT);
+
+    $fragment = $document->createDocumentFragment();
+    $fragment->appendXML("<ul><li>inScope</li></ul>");
+
+    $document->querySelector("span#replaceWithSUT")->replaceWith($fragment);
+
+    $actualResult = $fragment->querySelector("li");
+    $this->assertEquals("inScope", $actualResult->textContent);
+}
+
+public function testQuerySelectorAllAfterAddingToDocument() {
+    $document = new HTMLDocument(
+        self::DOC_CONTENT_BEFORE_INSERT);
+
+    $fragment = $document->createDocumentFragment();
+    $fragment->appendXML("<ul><li>inScope</li></ul>");
+
+    $document->querySelector("span#replaceWithSUT")->replaceWith($fragment);
+
+    $actualResult = $fragment->querySelectorAll("li");
+    $this->assertCount(1, $actualResult);
+    $this->assertEquals("inScope", $actualResult[0]->textContent);
 }
 
 }#
