e1af72545bf19dbe341c4a32d45352b675d0067f
Greg Bowler <greg.bowler@g105b.com>
2017-07-14 22:09:52 +0100

    Use scope resolution operator everywhere, closes #89

--- test/XMLDocument.test.php
+++ test/XMLDocument.test.php
@@ -6,17 +6,17 @@ class XMLDocumentTest extends \PHPUnit_Framework_TestCase {
 public function testConstruction() {
 	// test construction from raw XML
 	$fromRawXML = new XMLDocument(test\Helper::XML);
-	$this->assertInstanceOf('Gt\Dom\XMLDocument', $fromRawXML);
+	$this->assertInstanceOf(XMLDocument::class, $fromRawXML);
 	// test construction from a DOMDocument object
 	$domDocument = new \DOMDocument('1.0', 'UTF-8');
 	$domDocument->loadXML(test\Helper::XML);
 
 	$fromDOMDocument = new XMLDocument($domDocument);
-	$this->assertInstanceOf('Gt\Dom\XMLDocument', $fromDOMDocument);
+	$this->assertInstanceOf(XMLDocument::class, $fromDOMDocument);
 
 	// test construction from a XMLDocument object, just to be sure
 	$fromXMLDocument = new XMLDocument($fromRawXML);
-	$this->assertInstanceOf('Gt\Dom\XMLDocument', $fromXMLDocument);
+	$this->assertInstanceOf(XMLDocument::class, $fromXMLDocument);
 
 }
 
