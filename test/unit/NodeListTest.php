e625c6b33d9c700b5ae7d20043696d75c0dcd52a
Greg Bowler <greg.bowler@g105b.com>
2018-01-20 14:51:33 +0000

    Return null for out of bounds index Fixes #102

--- test/unit/NodeListTest.php
+++ test/unit/NodeListTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Dom\Test;
 
+use Gt\Dom\Element;
 use Gt\Dom\HTMLDocument;
 use Gt\Dom\NodeList;
 use Gt\Dom\Test\Helper\Helper;
@@ -19,5 +20,20 @@ class NodeListTest extends TestCase {
 				$object,
 				"$key instance of " . gettype($object));
 		}
+	}
+
+	public function testNodeListIndexOutOfBounds() {
+		$document = new HTMLDocument(Helper::HTML_MORE);
+		$paragraphList = $document->getElementsByTagName("p");
+		$count = count($paragraphList);
+
+		for($i = 0; $i < $count; $i++) {
+			self::assertInstanceOf(
+				Element::class,
+				$paragraphList[$i]
+			);
+		}
+
+		self::assertNull($paragraphList[$count]);
 	}
 }
