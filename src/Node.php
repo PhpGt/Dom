b5dd22413b43b482225ce983b0c7082679f2d55f
Greg Bowler <greg.bowler@g105b.com>
2021-02-19 10:45:59 +0000

    Add documentation to Node

--- src/Node.php
+++ src/Node.php
@@ -4,8 +4,25 @@ namespace Gt\Dom;
 use Gt\PropFunc\MagicProp;
 
 /**
- * A Node is an interface from which a number of DOM types inherit, and allows
- * these various types to be treated (or tested) similarly.
+ * The DOM Node interface is an abstract base class upon which many other DOM
+ * API objects are based, thus letting those object types to be used similarly
+ * and often interchangeably. As an abstract class, there is no such thing as a
+ * plain Node object. All objects that implement Node functionality are based
+ * on one of its subclasses. Most notable are Document, Element, and
+ * DocumentFragment.
+ *
+ * In addition, every kind of DOM node is represented by an interface based on
+ * Node. These include Attr, CharacterData (which Text, Comment, and
+ * CDATASection are all based on), ProcessingInstruction, DocumentType,
+ * Notation, Entity, and EntityReference.
+ *
+ * In some cases, a particular feature of the base Node interface may not apply
+ * to one of its child interfaces; in that case, the inheriting node may return
+ * null or throw an exception, depending on circumstances. For example,
+ * attempting to add children to a node type that cannot have children will
+ * throw an exception.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/Node
  *
  * @property-read string $baseURI Returns a DOMString representing the base URL of the document containing the Node
  * @property-read NodeList $childNodes Returns a live NodeList containing all the children of this node (including elements, text and comments). NodeList being live means that if the children of the Node change, the NodeList object is automatically updated.
@@ -24,9 +41,6 @@ use Gt\PropFunc\MagicProp;
  */
 class Node {
 	use MagicProp;
-//	use NonDocumentTypeChildNode;
-//	use ChildNode;
-//	use ParentNode;
 
 	protected function __prop_get_baseURI():string {
 
@@ -99,6 +113,10 @@ class Node {
 	 * current node. If the argument referenced an existing node on the
 	 * DOM tree, the node will be detached from its current position and
 	 * attached at the new position.
+	 *
+	 * @param Node $newNode
+	 * @return Node
+	 * @link
 	 */
 	public function appendChild(Node $newNode):Node {
 
@@ -107,6 +125,10 @@ class Node {
 	/**
 	 * Clone a Node, and optionally, all of its contents. By default, it
 	 * clones the content of the node.
+	 *
+	 * @param bool $deep
+	 * @return Node
+	 * @link
 	 */
 	public function cloneNode(bool $deep = false):Node {
 
@@ -115,6 +137,10 @@ class Node {
 	/**
 	 * Compares the position of the current node against another node in
 	 * any other document.
+	 *
+	 * @param Node $otherNode
+	 * @return int
+	 * @link
 	 */
 	public function compareDocumentPosition(Node $otherNode):int {
 
@@ -123,6 +149,10 @@ class Node {
 	/**
 	 * Returns a Boolean value indicating whether or not a node is a
 	 * descendant of the calling node.
+	 *
+	 * @param Node $otherNode
+	 * @return bool
+	 * @link
 	 */
 	public function contains(Node $otherNode):bool {
 
@@ -130,6 +160,9 @@ class Node {
 
 	/**
 	 * Returns the context object's root.
+	 *
+	 * @return Node
+	 * @link
 	 */
 	public function getRootNode():Node {
 
@@ -138,6 +171,9 @@ class Node {
 	/**
 	 * Returns a Boolean indicating whether or not the element has any
 	 * child nodes.
+	 *
+	 * @return bool
+	 * @link
 	 */
 	public function hasChildNodes():bool {
 
@@ -146,6 +182,11 @@ class Node {
 	/**
 	 * Inserts a Node before the reference node as a child of a
 	 * specified parent node.
+	 *
+	 * @param Node $newNode
+	 * @param Node $refNode
+	 * @return Node
+	 * @link
 	 */
 	public function insertBefore(Node $newNode, Node $refNode):Node {
 
@@ -155,6 +196,10 @@ class Node {
 	 * Accepts a namespace URI as an argument and returns a Boolean with a
 	 * value of true if the namespace is the default namespace on the given
 	 * node or false if not.
+	 *
+	 * @param string $namespace
+	 * @return bool
+	 * @link
 	 */
 	public function isDefaultNamespace(string $namespace):bool {
 
@@ -163,6 +208,10 @@ class Node {
 	/**
 	 * Returns a Boolean which indicates whether or not two nodes are of
 	 * the same type and all their defining data points match.
+	 *
+	 * @param Node $otherNode
+	 * @return bool
+	 * @link
 	 */
 	public function isEqualNode(Node $otherNode):bool {
 
@@ -171,6 +220,10 @@ class Node {
 	/**
 	 * Returns a Boolean value indicating whether or not the two nodes are
 	 * the same (that is, they reference the same object).
+	 *
+	 * @param Node $otherNode
+	 * @return bool
+	 * @link
 	 */
 	public function isSameNode(Node $otherNode):bool {
 
@@ -180,6 +233,9 @@ class Node {
 	 * Returns a DOMString containing the prefix for a given namespace URI,
 	 * if present, and null if not. When multiple prefixes are possible,
 	 * the result is implementation-dependent.
+	 *
+	 * @return ?string
+	 * @link
 	 */
 	public function lookupPrefix():?string {
 
@@ -189,6 +245,10 @@ class Node {
 	 * Accepts a prefix and returns the namespace URI associated with it on
 	 * the given node if found (and null if not). Supplying null for the
 	 * prefix will return the default namespace.
+	 *
+	 * @param ?string $prefix
+	 * @return ?string
+	 * @link
 	 */
 	public function lookupNamespaceURI(string $prefix = null):?string {
 
@@ -197,6 +257,8 @@ class Node {
 	/**
 	 * Clean up all the text nodes under this element (merge adjacent,
 	 * remove empty).
+	 *
+	 * @link
 	 */
 	public function normalize():void {
 
@@ -205,6 +267,10 @@ class Node {
 	/**
 	 * Removes a child node from the current element, which must be a
 	 * child of the current node.
+	 *
+	 * @param Node $oldNode
+	 * @return Node
+	 * @link
 	 */
 	public function removeChild(Node $oldNode):Node {
 
@@ -213,6 +279,11 @@ class Node {
 	/**
 	 * Replaces one child Node of the current one with the second one given
 	 * in parameter.
+	 *
+	 * @param Node $newNode
+	 * @param Node $oldNode
+	 * @return Node
+	 * @link
 	 */
 	public function replaceChild(Node $newNode, Node $oldNode):Node {
 
