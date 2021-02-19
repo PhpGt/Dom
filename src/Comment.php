e120d1ebb72e851e3ae284dcbb3871ff1f660789
Greg Bowler <greg.bowler@g105b.com>
2021-02-19 12:06:59 +0000

    Remove v2 classes

--- src/Comment.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Gt\Dom;
-
-/**
- * Represents textual notations within markup; although it is generally not
- * visually shown, such comments are available to be read in the source view.
- * Comments are represented in HTML and XML as content between '<!--' and '-->'.
- */
-class Comment extends \DOMComment {}
