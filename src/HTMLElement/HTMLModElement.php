63420ca7869aae0196d1ffe7915272491823fb91
Greg Bowler <greg.bowler@g105b.com>
2021-02-22 18:10:09 +0000

    Introduce HTMLModElement

---
+++ src/HTMLElement/HTMLModElement.php
@@ -0,0 +1,35 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLModElement interface provides special properties (beyond the regular
+ * methods and properties available through the HTMLElement interface they also
+ * have available to them by inheritance) for manipulating modification
+ * elements, that is <del> and <ins>.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLModElement
+ *
+ * @property string $cite Is a DOMString reflecting the cite HTML attribute, containing a URI of a resource explaining the change.
+ * @property string $dateTime Is a DOMString reflecting the datetime HTML attribute, containing a date-and-time string representing a timestamp for the change.
+ */
+class HTMLModElement extends HTMLElement {
+	/** https://developer.mozilla.org/en-US/docs/Web/API/HTMLModElement/cite */
+	public function __prop_get_cite():string {
+
+	}
+
+	/** https://developer.mozilla.org/en-US/docs/Web/API/HTMLModElement/cite */
+	public function __prop_set_cite(string $value):void {
+
+	}
+
+	/** https://developer.mozilla.org/en-US/docs/Web/API/HTMLModElement/dateTime */
+	public function __prop_get_dateTime():string {
+
+	}
+
+	/** https://developer.mozilla.org/en-US/docs/Web/API/HTMLModElement/dateTime */
+	public function __prop_set_dateTime(string $value):void {
+
+	}
+}
