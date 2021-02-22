02c220c970cb550af4d75028e55e5f08a37401f6
Greg Bowler <greg.bowler@g105b.com>
2021-02-22 18:03:30 +0000

    Introduce HTMLMetaElement

---
+++ src/HTMLElement/HTMLMetaElement.php
@@ -0,0 +1,39 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLMetaElement interface contains descriptive metadata about a document.
+ * It inherits all of the properties and methods described in the HTMLElement
+ * interface.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLMetaElement
+ *
+ * @property string $content Gets or sets the value of meta-data property.
+ * @property string $httpEquiv Gets or sets the name of an HTTP response header to define for a document.
+ * @property string $name Gets or sets the name of a meta-data property to define for a document.
+ */
+class HTMLMetaElement extends HTMLElement {
+	protected function __prop_get_content():string {
+
+	}
+
+	protected function __prop_set_content(string $value):void {
+
+	}
+
+	protected function __prop_get_httpEquiv():string {
+
+	}
+
+	protected function __prop_set_httpEquiv(string $value):void {
+
+	}
+
+	protected function __prop_get_name():string {
+
+	}
+
+	protected function __prop_set_name(string $value):void {
+
+	}
+}
