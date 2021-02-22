46051b3a6a1ea7965b40109afaa04ea0317f31ca
Greg Bowler <greg.bowler@g105b.com>
2021-02-22 15:02:31 +0000

    Introduce HTMLEmbedElement

---
+++ src/HTMLElement/HTMLEmbedElement.php
@@ -0,0 +1,56 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLEmbedElement interface provides special properties (beyond the
+ * regular HTMLElement interface it also has available to it by inheritance)
+ * for manipulating <embed> elements.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement
+ *
+ * @property int $height Is a DOMString reflecting the height HTML attribute, containing the displayed height of the resource.
+ * @property string $src Is a DOMString that reflects the src HTML attribute, containing the address of the resource.
+ * @property string $type Is a DOMString that reflects the type HTML attribute, containing the type of the resource.
+ * @property int $width Is a DOMString that reflects the width HTML attribute, containing the displayed width of the resource.
+ */
+class HTMLEmbedElement extends HTMLElement {
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/height */
+	protected function __prop_get_height():int {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/height */
+	protected function __prop_set_height(int $value):void {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/src */
+	protected function __prop_get_src():string {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/src */
+	protected function __prop_set_src(string $value):void {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/type */
+	protected function __prop_get_type():string {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/type */
+	protected function __prop_set_type(string $value):void {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/width */
+	protected function __prop_get_width():int {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLEmbedElement/width */
+	protected function __prop_set_width(int $value):void {
+
+	}
+}
