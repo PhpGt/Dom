92a1dac79f51e2425be96e5f28bceb5d16014148
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 21:35:38 +0000

    Introduce HTMLVideoElement

--- src/HTMLElement/HTMLVideoElement.php
+++ src/HTMLElement/HTMLVideoElement.php
@@ -13,9 +13,23 @@ namespace Gt\Dom\HTMLElement;
  *
  * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement
  *
+ * @property string $height Is a DOMString that reflects the height HTML attribute, which specifies the height of the display area, in CSS pixels.
  * @property string $poster Is a DOMString that reflects the poster HTML attribute, which specifies an image to show while no video data is available.
+ * @property-read int $videoHeight Returns an unsigned integer value indicating the intrinsic height of the resource in CSS pixels, or 0 if no media is available yet.
+ * @property-read int $videoWidth Returns an unsigned integer value indicating the intrinsic width of the resource in CSS pixels, or 0 if no media is available yet.
+ * @property string $width Is a DOMString that reflects the width HTML attribute, which specifies the width of the display area, in CSS pixels.
  */
 class HTMLVideoElement extends HTMLMediaElement {
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/height */
+	protected function __prop_get_height():string {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/height */
+	protected function __prop_set_height(string $value):void {
+
+	}
+
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/poster */
 	protected function __prop_get_poster():string {
 
@@ -23,6 +37,26 @@ class HTMLVideoElement extends HTMLMediaElement {
 
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/poster */
 	protected function __prop_set_poster(string $value):void {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoHeight */
+	protected function __prop_get_videoHeight():int {
+		return 0;
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/videoWidth */
+	protected function __prop_get_videoWidth():int {
+		return 0;
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/width */
+	protected function __prop_get_width():string {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLVideoElement/width */
+	protected function __prop_set_width(string $value):void {
 
 	}
 }
