fafc7bec28bde365c6b9d88fa0f49a812c2dbbc2
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 11:21:28 +0000

    Introduce HTMLParagraphElement

---
+++ src/HTMLElement/HTMLParagraphElement.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLParagraphElement interface provides special properties (beyond
+ * those of the regular HTMLElement object interface it inherits) for
+ * manipulating <p> elements.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLParagraphElement
+ */
+class HTMLParagraphElement extends HTMLElement {
+
+}
