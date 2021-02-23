afeddf28dd22589cdc7538f7c3b322d7c306dafc
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 10:01:17 +0000

    Introduce HTMLOptGroupElement

---
+++ src/HTMLElement/HTMLOptGroupElement.php
@@ -0,0 +1,35 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLOptGroupElement interface provides special properties and methods
+ * (beyond the regular HTMLElement object interface they also have available to
+ * them by inheritance) for manipulating the layout and presentation of
+ * <optgroup> elements.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptGroupElement
+ *
+ * @property bool $disabled Is a boolean representing whether or not the whole list of children <option> is disabled (true) or not (false).
+ * @property string $label Is a DOMString representing the label for the group.
+ */
+class HTMLOptGroupElement extends HTMLElement {
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptGroupElement/disabled */
+	protected function __prop_get_disabled():bool {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptGroupElement/disabled */
+	protected function __prop_set_disabled(bool $value):void {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptGroupElement/label */
+	protected function __prop_get_label():string {
+
+	}
+
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLOptGroupElement/label */
+	protected function __prop_set_label(string $value):void {
+
+	}
+}
