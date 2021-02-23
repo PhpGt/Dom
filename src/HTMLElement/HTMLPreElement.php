610e7ccf70e4164f3a618d2e9901c82f88fa98ea
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 16:36:36 +0000

    Introduce HTMLPreElement

---
+++ src/HTMLElement/HTMLPreElement.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLPreElement interface exposes specific properties and methods (beyond
+ * those of the HTMLElement interface it also has available to it by
+ * inheritance) for manipulating a block of preformatted text (<pre>).
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLPreElement
+ */
+class HTMLPreElement extends HTMLElement {
+
+}
