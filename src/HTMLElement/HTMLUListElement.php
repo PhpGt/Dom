e82c0d0aabc4864b94d70b61ffa6a2383ed6150a
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 21:31:13 +0000

    Introduce HTMLVideoElement

---
+++ src/HTMLElement/HTMLUListElement.php
@@ -0,0 +1,13 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLUListElement interface provides special properties (beyond those
+ * defined on the regular HTMLElement interface it also has available to it by
+ * inheritance) for manipulating unordered list elements.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLUListElement
+ */
+class HTMLUListElement extends HTMLElement {
+
+}
