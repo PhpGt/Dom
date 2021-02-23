8f038366cfdb1f210a309ceafce687aaab25ed37
Greg Bowler <greg.bowler@g105b.com>
2021-02-23 11:44:58 +0000

    Introduce HTMLPictureElement

---
+++ src/HTMLElement/HTMLPictureElement.php
@@ -0,0 +1,12 @@
+<?php
+namespace Gt\Dom\HTMLElement;
+
+/**
+ * The HTMLPictureElement interface represents a <picture> HTML element. It
+ * doesn't implement specific properties or methods.
+ *
+ * @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLPictureElement
+ */
+class HTMLPictureElement extends HTMLElement {
+
+}
