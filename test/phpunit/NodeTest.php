3546839f929bc0e3efd4867242bef58ca70c99b3
Greg Bowler <greg.bowler@g105b.com>
2021-02-24 12:21:31 +0000

    Rename test

--- test/phpunit/NodeTest.php
+++ test/phpunit/NodeTest.php
@@ -6,7 +6,7 @@ use Gt\Dom\Node;
 use PHPUnit\Framework\TestCase;
 
 class NodeTest extends TestCase {
-	public function testPrivateConstructor() {
+	public function testCanNotConstruct() {
 		self::expectException(Error::class);
 		self::expectExceptionMessage("Cannot instantiate abstract class Gt\Dom\Node");
 		$sut = new Node();
