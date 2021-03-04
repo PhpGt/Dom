ec5fb0c70e51cfec1442a3783c9267b9dcfed090
Greg Bowler <greg.bowler@g105b.com>
2021-03-04 15:54:30 +0000

    Test DocumentType accessors

--- test/phpunit/DocumentTypeTest.php
+++ test/phpunit/DocumentTypeTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Dom\Test;
 
+use Gt\Dom\Document;
 use Gt\Dom\Test\TestFactory\DocumentTestFactory;
 use PHPUnit\Framework\TestCase;
 
@@ -19,5 +20,25 @@ class DocumentTypeTest extends TestCase {
 		$sut = DocumentTestFactory::createHTMLDocument()->doctype;
 		$xmlType = DocumentTestFactory::createHTMLDocument(DocumentTestFactory::XML_BOOK)->doctype;
 		self::assertFalse($sut->isEqualNode($xmlType));
+	}
+
+	public function testNameHTML():void {
+		$sut = DocumentTestFactory::createHTMLDocument()->doctype;
+		self::assertEquals("html", $sut->name);
+	}
+
+	public function testNameXML():void {
+		$sut = DocumentTestFactory::createXMLDocument(DocumentTestFactory::XML_BOOK)->doctype;
+		self::assertEquals("book", $sut->name);
+	}
+
+	public function testPublicId():void {
+		$sut = DocumentTestFactory::createHTMLDocument()->doctype;
+		self::assertEquals("", $sut->publicId);
+	}
+
+	public function testSystemId():void {
+		$sut = DocumentTestFactory::createHTMLDocument()->doctype;
+		self::assertEquals("", $sut->systemId);
 	}
 }
