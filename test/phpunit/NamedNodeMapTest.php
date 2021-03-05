0bb5b65c05a818eb3dbd0762af6d8255973839bc
Greg Bowler <greg.bowler@g105b.com>
2021-03-05 15:52:42 +0000

    Test NamedNodeMap::getNamedItem()

--- test/phpunit/NamedNodeMapTest.php
+++ test/phpunit/NamedNodeMapTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Dom\Test;
 
+use Gt\Dom\Attr;
 use Gt\Dom\Document;
 use Gt\Dom\Element;
 use Gt\Dom\Facade\DOMDocumentFacade;
@@ -18,5 +19,28 @@ class NamedNodeMapTest extends TestCase {
 		$sut = NamedNodeMapFactory::create(fn() => $nativeElement->attributes, $element);
 		self::assertEquals(2, $sut->length);
 		self::assertCount(2, $sut);
+	}
+
+	public function testGetNamedItem():void {
+		$document = new DOMDocumentFacade(new Document());
+		$nativeElement = $document->createElement("example");
+		$nativeElement->setAttribute("one", "abc");
+		$nativeElement->setAttribute("two", "xyz");
+		/** @var Element $element */
+		$element = $document->getGtDomNode($nativeElement);
+		$sut = NamedNodeMapFactory::create(fn() => $nativeElement->attributes, $element);
+		$item = $sut->getNamedItem("two");
+		self::assertInstanceOf(Attr::class, $item);
+		self::assertEquals("xyz", $item->value);
+	}
+
+	public function testGetNamedItemNone():void {
+		$document = new DOMDocumentFacade(new Document());
+		$nativeElement = $document->createElement("example");
+		/** @var Element $element */
+		$element = $document->getGtDomNode($nativeElement);
+		$sut = NamedNodeMapFactory::create(fn() => $nativeElement->attributes, $element);
+		$item = $sut->getNamedItem("two");
+		self::assertNull($item);
 	}
 }
