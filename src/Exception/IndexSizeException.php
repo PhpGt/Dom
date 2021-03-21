a1a46c8be6708747fbd63fa6ca5419ebf84860c8
Simon <simon.speich@gmail.com>
2021-03-21 14:57:22 +0100

    add new exception when table row index is out of bounds

---
+++ src/Exception/IndexSizeException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Gt\Dom\Exception;
+
+/**
+ * Class IndexSizeException
+ * @package Gt\Dom\Exception
+ */
+class IndexSizeException extends DOMException
+{
+    protected $name = 'IndexSizeError';
+}
