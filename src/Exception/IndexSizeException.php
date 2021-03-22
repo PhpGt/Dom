3b785667527677935cc51aca183cb59594d13213
Greg Bowler <greg.bowler@g105b.com>
2021-03-22 11:16:02 +0000

    refactor: use MagicProp trait to make name property read only

--- src/Exception/IndexSizeException.php
+++ src/Exception/IndexSizeException.php
@@ -2,11 +2,7 @@
 
 namespace Gt\Dom\Exception;
 
-/**
- * Class IndexSizeException
- * @package Gt\Dom\Exception
- */
 class IndexSizeException extends DOMException
 {
-    protected $name = 'IndexSizeError';
+    protected string $pName = 'IndexSizeError';
 }
