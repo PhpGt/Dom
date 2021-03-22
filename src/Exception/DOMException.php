57255866bd64780d73000a0f2196e322080446d6
Greg Bowler <greg.bowler@g105b.com>
2021-03-22 11:16:02 +0000

    refactor: use MagicProp trait to make name property read only

--- src/Exception/DOMException.php
+++ src/Exception/DOMException.php
@@ -2,20 +2,25 @@
 
 namespace Gt\Dom\Exception;
 
+use Gt\PropFunc\MagicProp;
 use RuntimeException;
 
 
 /**
- * Class DOMException
  * The DOMException interface represents an abnormal event (called an exception) that occurs as a result of calling
  * a method or accessing a property of a web API. This is basically how error conditions are described in web APIs.
  *
  * Each exception has a name, which is a short "PascalCase"-style string identifying the error or abnormal condition.
  * @property-read string $name Returns a DOMString that contains one of the strings associated with an error name.
  * @link https://developer.mozilla.org/en-US/docs/Web/API/DOMException
- * @package Gt\Dom\Exception
  */
 class DOMException extends RuntimeException
 {
-    protected $name;
+    use MagicProp;
+
+    protected string $pName;
+
+    public function __prop_get_name():string {
+    	return $this->pName;
+    }
 }
