56bf4f13c31619d5d0643cc53792a59ab5f1ee16
Greg Bowler <greg.bowler@g105b.com>
2021-04-08 12:19:29 +0100

    feat: HTMLCanvasElement width/height property

--- src/HTMLElement/HTMLCanvasElement.php
+++ src/HTMLElement/HTMLCanvasElement.php
@@ -13,23 +13,28 @@ namespace Gt\Dom\HTMLElement;
  * @property int $width The width HTML attribute of the <canvas> element is a positive integer reflecting the number of logical pixels (or RGBA values) going across one row of the canvas. When the attribute is not specified, or if it is set to an invalid value, like a negative, the default value of 300 is used. If no [separate] CSS width is assigned to the <canvas>, then this value will also be used as the width of the canvas in the length-unit CSS Pixel.
  */
 class HTMLCanvasElement extends HTMLElement {
+	const DEFAULT_WIDTH = 300;
+	const DEFAULT_HEIGHT = 150;
+
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/height */
 	protected function __prop_get_height():int {
-
+		return $this->getAttribute("height")
+			?? self::DEFAULT_HEIGHT;
 	}
 
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/height */
 	protected function __prop_set_height(int $value):void {
-
+		$this->setAttribute("height", (string)$value);
 	}
 
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/width */
 	protected function __prop_get_width():int {
-
+		return $this->getAttribute("width")
+			?? self::DEFAULT_WIDTH;
 	}
 
 	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/width */
 	protected function __prop_set_width(int $value):void {
-
+		$this->setAttribute("width", (string)$value);
 	}
 }
