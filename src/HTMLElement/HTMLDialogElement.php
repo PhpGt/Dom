86582cd650d17bd891a5170cc3d896c6a54a61c2
Greg Bowler <greg.bowler@g105b.com>
2021-04-08 12:40:26 +0100

    feat: HTMLDialogElement::returnValue property

--- src/HTMLElement/HTMLDialogElement.php
+++ src/HTMLElement/HTMLDialogElement.php
@@ -1,6 +1,8 @@
 <?php
 namespace Gt\Dom\HTMLElement;
 
+use Gt\Dom\Exception\FunctionalityNotAvailableOnServerException;
+
 /**
  * The HTMLDialogElement interface provides methods to manipulate <dialog>
  * elements. It inherits properties and methods from the HTMLElement interface.
@@ -11,10 +13,12 @@ namespace Gt\Dom\HTMLElement;
  * @property string $returnValue A DOMString that sets or returns the return value for the dialog.
  */
 class HTMLDialogElement extends HTMLElement {
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/open */
 	protected function __prop_get_open():bool {
 		return $this->hasAttribute("open");
 	}
 
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/open */
 	protected function __prop_set_open(bool $value):void {
 		if($value) {
 			$this->setAttribute("open","");
@@ -24,11 +28,13 @@ class HTMLDialogElement extends HTMLElement {
 		}
 	}
 
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/returnValue */
 	protected function __prop_get_returnValue():string {
-
+		throw new FunctionalityNotAvailableOnServerException("returnValue");
 	}
 
+	/** @link https://developer.mozilla.org/en-US/docs/Web/API/HTMLDialogElement/returnValue */
 	protected function __prop_set_returnValue(string $value):void {
-
+		throw new FunctionalityNotAvailableOnServerException("returnValue");
 	}
 }
--- test/phpunit/HTMLElement/HTMLDialogElementTest.php
+++ test/phpunit/HTMLElement/HTMLDialogElementTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Gt\Dom\Test\HTMLElement;
 
+use Gt\Dom\Exception\FunctionalityNotAvailableOnServerException;
 use Gt\Dom\HTMLElement\HTMLDialogElement;
 use Gt\Dom\Test\TestFactory\NodeTestFactory;
 
@@ -21,5 +22,20 @@ class HTMLDialogElementTest extends HTMLElementTestCase {
 		$sut->open = false;
 		self::assertFalse($sut->open);
 		self::assertFalse($sut->hasAttribute("open"));
+	}
+
+	public function testReturnValueGetter():void {
+		/** @var HTMLDialogElement $sut */
+		$sut = NodeTestFactory::createHTMLElement("dialog");
+		self::expectException(FunctionalityNotAvailableOnServerException::class);
+		/** @noinspection PhpUnusedLocalVariableInspection */
+		$something = $sut->returnValue;
+	}
+
+	public function testReturnValueSetter():void {
+		/** @var HTMLDialogElement $sut */
+		$sut = NodeTestFactory::createHTMLElement("dialog");
+		self::expectException(FunctionalityNotAvailableOnServerException::class);
+		$sut->returnValue = "something";
 	}
 }
