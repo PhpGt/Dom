8b856f571da28d54c640ff376a34c9a24b61b4f7
Greg Bowler <greg.bowler@g105b.com>
2022-05-29 16:48:10 +0100

    feature: DocumentFragment::getElementById

--- src/DocumentFragment.php
+++ src/DocumentFragment.php
@@ -2,7 +2,21 @@
 namespace Gt\Dom;
 
 use DOMDocumentFragment;
+use DOMXPath;
 
 class DocumentFragment extends DOMDocumentFragment {
 	use RegisteredNodeClass;
+
+	/**
+	 * Returns the first Element node within the DocumentFragment, in
+	 * document order, that matches the specified ID. Functionally
+	 * equivalent to Document.getElementById().
+	 *
+	 * @link https://developer.mozilla.org/en-US/docs/Web/API/DocumentFragment/getElementById
+	 */
+	public function getElementById(string $id):null|Node|Element {
+		$xpath = new DOMXPath($this->ownerDocument);
+		$result = $xpath->evaluate("*[@id='$id']", $this);
+		return $result?->item(0);
+	}
 }
--- test/phpunit/DocumentFragmentTest.php
+++ test/phpunit/DocumentFragmentTest.php
@@ -2,20 +2,23 @@
 namespace Gt\Dom\Test;
 
 use Gt\Dom\DocumentFragment;
+use Gt\Dom\HTMLDocument;
 use Gt\Dom\Test\TestFactory\NodeTestFactory;
 use PHPUnit\Framework\TestCase;
 
 class DocumentFragmentTest extends TestCase {
-//	public function testGetElementByIdEmpty():void {
-//		$sut = NodeTestFactory::createFragment();
-//		self::assertNull($sut->getElementById("nothing"));
-//	}
-//
-//	public function testGetElementById():void {
-//		$sut = NodeTestFactory::createFragment();
-//		$nodeWithId = $sut->ownerDocument->createElement("div");
-//		$nodeWithId->id = "test";
-//		$sut->appendChild($nodeWithId);
-//		self::assertSame($nodeWithId, $sut->getElementById("test"));
-//	}
+	public function testGetElementByIdEmpty():void {
+		$document = new HTMLDocument();
+		$sut = $document->createDocumentFragment();
+		self::assertNull($sut->getElementById("nothing"));
+	}
+
+	public function testGetElementById():void {
+		$document = new HTMLDocument();
+		$sut = $document->createDocumentFragment();
+		$nodeWithId = $sut->ownerDocument->createElement("div");
+		$nodeWithId->id = "test";
+		$sut->appendChild($nodeWithId);
+		self::assertSame($nodeWithId, $sut->getElementById("test"));
+	}
 }
