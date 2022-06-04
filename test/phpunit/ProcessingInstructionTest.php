09db1b04eb189c437aab272db2cafc1e508d3f4d
Greg Bowler <greg.bowler@g105b.com>
2022-06-04 10:42:15 +0100

    test: ProcessingInstruction

--- test/phpunit/ProcessingInstructionTest.php
+++ test/phpunit/ProcessingInstructionTest.php
@@ -3,20 +3,23 @@ namespace Gt\Dom\Test;
 
 use Gt\Dom\ProcessingInstruction;
 use Gt\Dom\Test\TestFactory\DocumentTestFactory;
+use Gt\Dom\XMLDocument;
 use PHPUnit\Framework\TestCase;
 
 class ProcessingInstructionTest extends TestCase {
-//	public function testIsEqualNode():void {
-//		$sut = DocumentTestFactory::createXMLDocument()->createProcessingInstruction("target", "data");
-//		$other = $sut->cloneNode();
-//		self::assertTrue($sut->isEqualNode($other));
-//	}
-//
-//	public function testIsEqualNodeNotEqual():void {
-//		$sut = DocumentTestFactory::createXMLDocument()->createProcessingInstruction("target", "data");
-//		/** @var ProcessingInstruction $other */
-//		$other = $sut->cloneNode();
-//		$other->data = "other-data";
-//		self::assertFalse($sut->isEqualNode($other));
-//	}
+	public function testIsEqualNode():void {
+		$document = new XMLDocument();
+		$sut = $document->createProcessingInstruction("target", "data");
+		$other = $sut->cloneNode();
+		self::assertTrue($sut->isEqualNode($other));
+	}
+
+	public function testIsEqualNodeNotEqual():void {
+		$document = new XMLDocument();
+		$sut = $document->createProcessingInstruction("target", "data");
+		/** @var ProcessingInstruction $other */
+		$other = $sut->cloneNode();
+		$other->data = "other-data";
+		self::assertFalse($sut->isEqualNode($other));
+	}
 }
