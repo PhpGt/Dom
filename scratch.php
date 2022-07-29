5095d89f58de19200b920c2fff39491c328bee9f
Greg Bowler <greg.bowler@g105b.com>
2022-07-29 10:34:29 +0100

    scratch: Marcin's test isolation

--- scratch.php
+++ scratch.php
@@ -1,51 +1,37 @@
 <?php
+use Gt\Dom\HTMLDocument;
 
-$document = new DOMDocument("1.0", "UTF-8");
-$document->registerNodeClass(DOMNode::class, MyDOMNode::class);
-$document->registerNodeClass(DOMElement::class, MyDOMElement::class);
-$document->registerNodeClass(DOMDocumentFragment::class, MyDOMDocumentFragment::class);
+require "vendor/autoload.php";
 
-$html = <<<HTML
-<!doctype html>
-<h1>Hello, <span>you</span>!</h1>
-
-<form method="post">
-	<label>
-		<span>Your name, please</span>
-		<input id="name-input" name="your-name" required autocomplete="off" autofocus />
-	</label>
+$content = <<<HTML
+<div>
+	<p id="testNodeText">
+		Hello, Marcin!
+	</p>
+	<button class="btn-textNodeTest">Button</button>
+</div>
+<script>
+{
+	const p = document.querySelector("#testNodeText");
+	const btn = document.querySelector(".btn-textNodeTest");
 	
-	<button name="do" value="greet">Greet!</button>
-</form>
+	const word1 = document.createTextNode("Psy");
+	p.append(word1);
+	
+	p.append(" są ");
+	
+	const word2 = document.createTextNode("fajne");
+	p.append(word2);
+	
+	btn.addEventListener("click", () => {
+		console.dir(word1);
+		
+		word1.textContent = "Koty też";
+		word2.textContent = "super!";
+	});
+}
+</script>
 HTML;
-$document->loadHTML($html);
 
-$input = $document->getElementById("name-input");
-
-var_dump($input);
-
-class DOMString implements Stringable {
-	public static function createFromCallback(Closure $param) {
-
-	}
-
-	public function __toString():string {
-
-	}
-}
-
-class MyDOMNode extends DOMNode {
-
-}
-
-class MyDOMElement extends DOMElement {
-	public string $tagName = "test";
-
-	public function __invoke() {
-		die(__METHOD__);
-	}
-}
-
-class MyDOMDocumentFragment extends DOMDocumentFragment {
-
-}
+$document = new HTMLDocument($content);
+echo $document;
