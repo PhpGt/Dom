bbf9171b4fe6667c71d4606e1fe0858eb4ab0ec3
Greg Bowler <greg.bowler@g105b.com>
2022-09-05 20:59:09 +0100

    maintenance: dependabot closes #391

--- scratch.php
+++ scratch.php
@@ -1,51 +1,20 @@
 <?php
 
-$document = new DOMDocument("1.0", "UTF-8");
-$document->registerNodeClass(DOMNode::class, MyDOMNode::class);
-$document->registerNodeClass(DOMElement::class, MyDOMElement::class);
-$document->registerNodeClass(DOMDocumentFragment::class, MyDOMDocumentFragment::class);
-
 $html = <<<HTML
 <!doctype html>
-<h1>Hello, <span>you</span>!</h1>
 
-<form method="post">
-	<label>
-		<span>Your name, please</span>
-		<input id="name-input" name="your-name" required autocomplete="off" autofocus />
-	</label>
-	
-	<button name="do" value="greet">Greet!</button>
-</form>
+<h1>Hello, <span>you</span>!</h1>
 HTML;
+
+// Create a new document with the above HTML.
+$document = new DOMDocument("1.0", "utf-8");
 $document->loadHTML($html);
 
-$input = $document->getElementById("name-input");
+// Get reference to span tag.
+$span = $document->getElementsByTagName("span")->item(0);
 
-var_dump($input);
+// Set the span's tag to user-supplied $name (malicious user can enter JavaScript!)
+$name = "<script>alert('XSS');</script>";
+$span->textContent = $name;
 
-class DOMString implements Stringable {
-	public static function createFromCallback(Closure $param) {
-
-	}
-
-	public function __toString():string {
-
-	}
-}
-
-class MyDOMNode extends DOMNode {
-
-}
-
-class MyDOMElement extends DOMElement {
-	public string $tagName = "test";
-
-	public function __invoke() {
-		die(__METHOD__);
-	}
-}
-
-class MyDOMDocumentFragment extends DOMDocumentFragment {
-
-}
+echo $document->saveHTML();
