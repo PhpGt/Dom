0d0d1691ab7a83cd4bb36633ef513c72d3f9c6a5
Greg Bowler <greg.bowler@g105b.com>
2023-11-30 14:05:17 +0000

    test: isolate bug #448

--- test/phpunit/DOMStringMapTest.php
+++ test/phpunit/DOMStringMapTest.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUndefinedFieldInspection */
 namespace Gt\Dom\Test;
 
 use Gt\Dom\DOMStringMap;
@@ -46,5 +46,23 @@ class DOMStringMapTest extends TestCase {
 		$sut->thisIsCamelCase = "example123";
 		self::assertSame("example123", $sut->get("thisIsCamelCase"));
 		self::assertSame("example123", $sut->get("this-is-camel-case"));
+	}
+
+	public function testSetter_withNumbers():void {
+		$keyValuePairs = [];
+		$getter = function() use (&$keyValuePairs) {
+			return $keyValuePairs;
+		};
+		$setter = function(array $kvp) use (&$keyValuePairs) {
+			$keyValuePairs = $kvp;
+		};
+		$sut = new DOMStringMap($getter, $setter);
+		$sut->example1 = "one";
+		$sut->example2 = "two";
+		$sut->example3 = "three";
+
+		self::assertSame("one", $sut->example1);
+		self::assertSame("two", $sut->example2);
+		self::assertSame("three", $sut->example3);
 	}
 }
